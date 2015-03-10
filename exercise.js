247f67fdbb39761b3dae22744359400ad1ca64cc
Elaine Trombley <elaine.trombley3@gmail.com>
2015-03-10 12:30:51 -0400

    chaining methods, no refresh delay

--- exercise.js
+++ exercise.js
@@ -1,66 +1,83 @@
 var cb = function(result) {
-    var items =  document.getElementById('items');
-    if (typeof(items) == 'undefined' || items == null) {
-      var items = document.createElement('div');
-        items.setAttribute('id','items')
-        var container = document.getElementById('statsContainer')
-        container.appendChild(items)
+    var container = document.getElementById('statsContainer');
+    var oldItems = document.getElementById('items');
+    var iObj;
+    if (oldItems === null) {
+        iObj = allMyFunctions.fetchData(result).sortByKeys().createDom().itemsObj;
+        container.appendChild(iObj)
+    } else {
+        iObj= allMyFunctions.fetchData(result).sortByKeys().createDom().itemsObj;
+        container.replaceChild(iObj, oldItems);
     }
-    var myResults = {}
-    for (var i=0; i < result.pages.length; i++) {
-        console.log(result.pages)
-        myResults[result.pages[i].stats.people] = result.pages[i].title
-    }
-    var sorted = sortByKeys(myResults)
-
-    Object.keys(sorted).reverse().forEach(function(v, i) {
-        console.log(v, sorted[v] + ' hey');
-        var items =  document.getElementById('items');
-        var item = document.createElement('div');
-        item.setAttribute('class', 'item');
-        item.addEventListener('click', function(){
-          openRefStats(result.pages[i].stats.toprefs);
-        })
-        items.appendChild(item);
-
-        var visits = document.createElement('span');
-        visits.setAttribute('class', 'visits');
-        visits.innerHTML = v;
-        item.appendChild(visits);
-        var title = document.createElement('span');
-        title.setAttribute('class', 'title');
-        title.innerHTML = sorted[v]
-        item.appendChild(title)
-
-    });
     window.setTimeout(function() {
-        var items = document.getElementById('items')
-        while (items.firstChild) {
-            items.removeChild(items.firstChild);
-        }
         cbApi.getData(cb);
     }, 5000);
 };
-    cbApi.getData(cb);
+
+cbApi.getData(cb);
+
+var allMyFunctions = {
+    array:{},
+    data:{},
+    itemsObj:{},
+
+    fetchData: function(result) {
+        var items =  document.getElementById('items');
+        var myResults = {};
+        for (var i=0; i < result.pages.length; i++) {
+            myResults[result.pages[i].stats.people] = result.pages[i].title
+        }
+        this.data = myResults;
+
+        return this;
+    },
+
+    sortByKeys: function() {
+        var input = this.data;
+        var arraykeys=[];
+        for(var k in input) {arraykeys.push(k);}
+        arraykeys.sort();
+
+        var newArray=[];
+        for(var i=arraykeys.length-1; i>=0; i--) {
+            newArray[arraykeys[i]] = input[arraykeys[i]];
+        }
+        this.array = newArray;
+        return this;
+    },
+
+    createDom: function() {
+        var array = this.array;
+        var datas = this.data;
 
 
-function sortByKeys(input) {
-    // debugger;
-  var arraykeys=[];
-  for(var k in input) {arraykeys.push(k);}
-  arraykeys.sort();
+        var items = document.createElement('div');
+        items.setAttribute('id','items')
 
-  var newArray=[];
-  for(var i=arraykeys.length-1; i>=0; i--) {
-      newArray[arraykeys[i]]=input[arraykeys[i]];
-  }
-  return newArray;
+        Object.keys(array).reverse().forEach(function(v, i) {
+            console.log(v, array[v] + ' hey');
+            var item = document.createElement('div');
+            item.setAttribute('class', 'item');
+            item.addEventListener('click', function(){
+                debugger;
+              this.openRefStats(datas.pages[i].stats.toprefs);
+            })
+            items.appendChild(item);
+            var visits = document.createElement('span');
+            visits.setAttribute('class', 'visits');
+            visits.innerHTML = v;
+            item.appendChild(visits);
+            var title = document.createElement('span');
+            title.setAttribute('class', 'title');
+            title.innerHTML = array[v]
+            item.appendChild(title)
+        })
+        this.itemsObj = items;
+        return this;
+    },
+
+    openRefStats:function(array) {
+        console.log('top refs', array )
+    }
+
 }
-
-var openRefStats= function(i) {
-}
-
-//load dom elements
-// update dom on callback
-// update numbers
-// change positions
