2f522c15272f3fad898d7344ba765d0de0a02519
Elaine Trombley <elaine.trombley3@gmail.com>
2015-03-09 23:33:06 -0400

    replaces data, long load time

--- exercise.js
+++ exercise.js
@@ -1,29 +1,66 @@
 var cb = function(result) {
-        for (var i=0; i < result.pages.length; i++){
-            console.log(result.pages[i].stats.people, result.pages[i].title)
-            var items = document.createElement('div')
-            items.setAttribute('class', 'items')
-            items.addEventListener('click', function(){
-              openRefStats()
-            })
-            statsContainer.appendChild(items);
-            var visits = document.createElement('span')
-            visits.setAttribute('class', 'visits')
-            visits.innerHTML = result.pages[i].stats.people
-            items.appendChild(visits)
-            var title = document.createElement('span')
-            title.setAttribute('class', 'title')
-            title.innerHTML = result.pages[i].title
-            items.appendChild(title)
+    var items =  document.getElementById('items');
+    if (typeof(items) == 'undefined' || items == null) {
+      var items = document.createElement('div');
+        items.setAttribute('id','items')
+        var container = document.getElementById('statsContainer')
+        container.appendChild(items)
+    }
+    var myResults = {}
+    for (var i=0; i < result.pages.length; i++) {
+        console.log(result.pages)
+        myResults[result.pages[i].stats.people] = result.pages[i].title
+    }
+    var sorted = sortByKeys(myResults)
 
+    Object.keys(sorted).reverse().forEach(function(v, i) {
+        console.log(v, sorted[v] + ' hey');
+        var items =  document.getElementById('items');
+        var item = document.createElement('div');
+        item.setAttribute('class', 'item');
+        item.addEventListener('click', function(){
+          openRefStats(result.pages[i].stats.toprefs);
+        })
+        items.appendChild(item);
+
+        var visits = document.createElement('span');
+        visits.setAttribute('class', 'visits');
+        visits.innerHTML = v;
+        item.appendChild(visits);
+        var title = document.createElement('span');
+        title.setAttribute('class', 'title');
+        title.innerHTML = sorted[v]
+        item.appendChild(title)
+
+    });
+    window.setTimeout(function() {
+        var items = document.getElementById('items')
+        while (items.firstChild) {
+            items.removeChild(items.firstChild);
         }
-        // window.setTimeout(function() {
-        //   cbApi.getData(cb);
-        // }, 5000);
-      };
-      cbApi.getData(cb);
+        cbApi.getData(cb);
+    }, 5000);
+};
+    cbApi.getData(cb);
 
 
-var openRefStats= function() {
-    console.log('open stats')
+function sortByKeys(input) {
+    // debugger;
+  var arraykeys=[];
+  for(var k in input) {arraykeys.push(k);}
+  arraykeys.sort();
+
+  var newArray=[];
+  for(var i=arraykeys.length-1; i>=0; i--) {
+      newArray[arraykeys[i]]=input[arraykeys[i]];
+  }
+  return newArray;
 }
+
+var openRefStats= function(i) {
+}
+
+//load dom elements
+// update dom on callback
+// update numbers
+// change positions
